c254d66a6ca0cb7e203677bf160b4e2e175f23b4
melaniev <melanie.vanhorn@gmail.com>
2013-04-09 18:12:11 -0400

    initial add

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html id='home-page'>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+
+	</head>
+	<body>
+		<div id='home-page-login-container'>
+			<form action=" " method="post">
+
+				<div id='pretty-login'>
+					<span class='login-label'>username: </span><input type='text' name='username' class='login-input' id='login-username'/><br />
+					<span class='login-label'>password: </span><input type='text' name='password' class='login-input' id='login-password'/>
+
+				</div>
+
+					<div style='margin-top:10px;'>
+					<input type='submit' value="Log In" name='home-log-in' class='button' style='display:block;float:right;'/>
+					</div>
+			</form>
+		</div>
+	</body>
+</html>
---
+++ lobby.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html id='lobby-page'>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+
+	</head>
+	<body>
+		<!-- Lobby Section - Welcome -->
+		<div class='lobby-container' id='welcome'>
+			<span>Logged in as, Username</span><a href='' id='log-out'>Log out</a>
+		</div>		
+		<!-- Lobby Section - Game Buttons -->
+		<div class='lobby-container' id='game-options'>
+			<h2>Games</h2>
+
+			<a href='' class='button'>New Game</a>
+			<a href='' class='button'>Join Game</a>
+			<a href='' class='button'>View Past Games</a>
+
+
+		</div>
+
+		<!-- Lobby Section - Recored-->
+		<div class='lobby-container' id='my-record'>
+			<h2>Records</h2>
+			<h3>Wins</h3>
+			<p>XX</p>
+			<h3>Losses</h3>
+			<p>XX</p>
+			<h3>Draws</h3>
+			<p>XX</p>
+			<h3>$$</h3>
+			<p>XX</p>
+		</div>
+
+	</body>
+</html>
